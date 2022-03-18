4fb79be2e61297ad1023aad4c91722fcd2d3b088
Orest Tokovenko <oresttokovenko@gmail.com>
2022-03-18 18:57:33 +0000

    update

--- models/staging/TPCH_SF1/orders_facts.sql
+++ models/staging/TPCH_SF1/orders_facts.sql
@@ -14,7 +14,6 @@ final AS (
         O_ORDERSTATUS as order_status,
         O_TOTALPRICE as total_price,
         O_ORDERDATE as order_date,
-        O_CLERK as clerk,
         O_SHIPPRIORITY as ship_priority
     FROM orders_facts
 )
